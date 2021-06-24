da6935b23835234d74324978746b7173c273d8ee
yahyaalbashar <v.salvatore7.gs@gmail.com>
2021-06-24 13:16:07 +0300

    reduce function

---
+++ class-05/demo/reduce.js
@@ -0,0 +1,27 @@
+'use strict';
+// let numbers = [1,2,3,4];
+// let sum = numbers.reduce( (accumulator,value) =>{
+//   accumulator = accumulator + value;
+//   console.log('acc: ',accumulator);
+//   console.log('val: ',value);
+//   return accumulator;
+// }, 5);
+
+// console.log(sum);
+// let people = [
+//     {name:'Fred', role:'Developer'},
+//     {name:'Suzy', role:'Developer'},
+//     {name:'Gina', role:'Manager'},
+//     {name:'Jim', role:'Support'}
+//   ];
+  
+//   let folks = people.reduce( (accumulator, person,idx) => {
+//     accumulator[person.name] = person.role;
+//     return accumulator;
+
+//   }, {} );
+
+//   console.log(folks.Gina);
+//   console.log(folks.Fred);
+//   console.log(folks.Suzy);
+//   console.log(folks.Jim);
